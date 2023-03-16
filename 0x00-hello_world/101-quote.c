898681154d693edcc9f071c76f7b34bd5513092a
youssef665 <yi559668@gmail.com>
2023-03-16 15:58:07 -0700

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ * UNix is basically simple
+ */
+
+int main(void)
+{
+	
+	char c[]="and that piece of art is useful" - Dora Korpar, 2015-10-19";
+	for(int i=0;c[i]!=NULL;i++)
+	{
+		putchar(c[i]);
+
+	}
+
+
+
+	return (1);
+}
