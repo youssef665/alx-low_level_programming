1431cc88789c627b3f1e84abb482f3f02d9a541a
youssef665 <yi559668@gmail.com>
2023-03-17 10:24:28 -0700

    -m

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 /*check if the number postive ,negative or zero*/
 
-/* main - Entry point
+/*
+ * main - Entry point
  *
  * Return: Always 0 
- * */
+ */
 int main(void)
 {
 	int n;
