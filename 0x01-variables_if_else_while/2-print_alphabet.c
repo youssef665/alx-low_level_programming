169013f10024bfc36f2d580252fd55290ab2ec99
youssef665 <yi559668@gmail.com>
2023-03-17 11:50:28 -0700

    program that prints the alphabet in lowercase, followed by a new line.program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+/*c program that prints the alphapets */
+
+int main(void){
+
+	for(int i = 'a'; i < 'z'; i++)
+		putchar(i);
+
+
+
+return (0);
+
+}
