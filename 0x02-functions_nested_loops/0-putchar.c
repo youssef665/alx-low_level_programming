c9ff8ee880edae876fa3e3d4cd462849f0a5014f
youssef665 <yi559668@gmail.com>
2023-03-21 10:22:53 -0700

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ * program to print _putchar */
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i = 0;
+
+	while ( arr[i] < 8 )
+	{
+		putchar(i);
+
+	}
+
+		putchar('\n');
+			
+		return (0);
+}
