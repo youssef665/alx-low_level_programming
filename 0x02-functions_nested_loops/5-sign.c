7a9e01efe999c04d8ce912801f63a4f436462d76
youssef665 <yi559668@gmail.com>
2023-03-22 13:37:12 -0700

    dd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,24 +13,18 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		putchar(',');
-		putchar(' ');
 
 		return (1);
 	}
 	else if (n == 0)
 	{
 		putchar('0');
-		putchar(',');
-		putchar(' ');
 
 		return (0);
 	}
 	else
 	{
 		putchar('-');
-		putchar(',');
-		putchar(' ');
 
 		return (-1);
 	}
