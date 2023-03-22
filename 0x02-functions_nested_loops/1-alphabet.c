6e08f52f90b67ae6933d661d70b7d0bda9d12eb1
youssef665 <yi559668@gmail.com>
2023-03-22 11:51:58 -0700

    d

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,8 +3,8 @@
 
 /**
  * main - check the code
- *
- * print_alphabet , a funtion that print the alphabet in lowercase
+ */
+/** print_alphabet , a funtion that print the alphabet in lowercase
  */
 
 void print_alphabet(void)
