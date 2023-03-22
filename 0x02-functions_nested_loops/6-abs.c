c93b223ea7ceb774cc67c13bdb1d5edb8c7c86d4
youssef665 <yi559668@gmail.com>
2023-03-22 14:35:48 -0700

    d

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of integers
+ *@int: Frist operand
+ *
+ * Return: always postive integer
+ */
+
+int _abs(int)
+{
+	int z;
+
+	if (z < 0)
+
+		return (z * -1);
+	else
+		return (z);
+}
