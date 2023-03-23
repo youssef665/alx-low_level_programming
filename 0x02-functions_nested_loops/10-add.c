af4e754d4adabc11c6fc9945e85bd4b788530228
youssef665 <yi559668@gmail.com>
2023-03-22 17:47:01 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - function that adds two numbers
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: integer value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,6 @@ int _abs(int);
 int print_last_digit(int);
 void times_table(void);
 void print_to_98(int n);
+int add(int, int);
 
 #endif
