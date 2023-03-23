d873139c5f101bd053b0e89ce91b019d62b4202f
youssef665 <yi559668@gmail.com>
2023-03-23 04:53:08 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test funciton that prints if integer is postive or negative
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	postive_or_negative(i);
+
+	return (0);
+}
