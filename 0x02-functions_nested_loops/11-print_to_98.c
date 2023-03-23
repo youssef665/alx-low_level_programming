7f22206795a8ea076697f8925b1a5b0e72190c69
youssef665 <yi559668@gmail.com>
2023-03-22 17:26:44 -0700

    d

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,11 +23,13 @@ void print_to_98(int n)
 			}
 				
 			printf(", ");
+	
 		}
+		printf("\n");
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i <= 98;i++)
 		{
 			printf("%d, ",i);
 			if (i == 98)
@@ -38,6 +40,7 @@ void print_to_98(int n)
 			printf(", ");
 		}
 	}
+	printf("\n");
 }
 
 
