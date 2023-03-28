6b55887b63fab262d5505e72417540549dee5686
youssef665 <yi559668@gmail.com>
2023-03-28 11:42:03 -0700

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while ( *(str + i) != '\0')
-	{	
+	while (*(str + i) != '\0')
+	{
 		if ((i % 2) == 0)
 		{
-		putchar(*(str +i));
+		putchar(*(str + i));
 		}
 
 		i++;
