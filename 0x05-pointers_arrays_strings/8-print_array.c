f6379776356a3cf7b22afb795a99978c5f1d33c3
youssef665 <yi559668@gmail.com>
2023-03-29 07:56:41 -0700

    last try

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,11 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		putchar(' ');
 
-		if (i != n-1)
+		if (i != n - 1)
 		{
 			putchar(',');
 		}
+		putchar(' ');
 	}
 }
