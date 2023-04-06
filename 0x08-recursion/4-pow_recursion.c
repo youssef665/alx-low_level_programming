04dd16089093a505f067c3b7d1b5db68e527e300
youssef665 <yi559668@gmail.com>
2023-04-06 11:17:14 -0700

    fixing betty error

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function that returns the value of x power of y
  *@x: first intger argument
  *@y: second integer argument
  *
@@ -14,10 +14,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	if ( y == 0)
+	if (y == 0)
 	{
 		return (1);
 	}
 
-	return (x * _pow_recursion(x, y-1));
+	return (x * _pow_recursion(x, y - 1));
 }
