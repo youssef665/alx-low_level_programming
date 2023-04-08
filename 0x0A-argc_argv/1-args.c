30c1f561935221e177fb5b065b2dccf4e087e67c
youssef665 <yi559668@gmail.com>
2023-04-08 11:52:54 -0700

    1st try

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: an integer argument counter
+ * @argv: a pointer to an array of strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	printf("%d\n", argc);
+
+	return (0);
+}
