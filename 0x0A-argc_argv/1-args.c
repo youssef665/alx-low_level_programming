ee48ef4db1d277a49f781c18abbb8c33cb7c640d
youssef665 <yi559668@gmail.com>
2023-04-08 12:27:26 -0700

    my first try but i think there is something wrong

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,6 +10,9 @@
 
 int main(int argc, char *argv[])
 {
+	int i = 0;
+
+	for (i = 0; i < argc; 
 
 	printf("%d\n", argc);
 
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - an Entry point
+ * @argc: is an argument counter
+ * @argv: is a pointer to array of strings argument
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	else
+	{
+		int i, mult = 1;
+
+		for (i = 1; i < argc; i++)
+		{
+			mult *= atoi(argv[i]);
+		}
+		printf("%d\n", mult);
+	}
+	return (0);
+}
