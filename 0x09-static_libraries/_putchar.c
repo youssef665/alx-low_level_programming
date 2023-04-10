1b2a3fd9a2cbe3b6068a808f057b8b33f232f24d
youssef665 <yi559668@gmail.com>
2023-04-10 14:49:00 -0700

    try

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ *
+ * program to print _putchar
+ */
+
+int main(void)
+{
+	char arr[] = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(arr[i]);
+		i++;
+	}
+
+		putchar('\n');
+
+		return (0);
+}
