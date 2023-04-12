e8d6f5b268ed5918033647d5905d3e1c6de95b7f
youssef665 <yi559668@gmail.com>
2023-04-12 06:39:16 -0700

    try again

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	else if (size != 0)
+	 if (size != 0)
 	{
 		ptr = (char *)malloc(size * sizeof(char));
 		if (ptr != NULL)
