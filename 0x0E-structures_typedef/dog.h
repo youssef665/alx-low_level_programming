785282cd3ff54822ff978ce086e2b00854fd0690
youssef665 <yi559668@gmail.com>
2023-04-17 13:49:52 -0700

    betty mistake

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - contains dog properties
+ * @name: pointer character to dog name
+ * @age: intger
+ * @owner: pointer to owner's name
+ *
+ * Description: struct that contains the proeries of the dog
+ */
+
 
 struct dog
 {
