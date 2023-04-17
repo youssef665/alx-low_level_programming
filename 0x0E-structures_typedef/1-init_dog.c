7c34bf474c7e8d2b5935c0f239c6edd480fade90
youssef665 <yi559668@gmail.com>
2023-04-17 13:58:41 -0700

    i think somthing is wrong

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+
+/**
+ * init_dog - a functions that intialize a variable of type struct dog
+ * @d: an argument pointer of type struct
+ * @name: an argument pointer character
+ * @age: an argument integer
+ * @owner: an argument pointer character
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+};
