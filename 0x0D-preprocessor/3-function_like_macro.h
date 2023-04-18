8eab9e6d879c43a2f12ef0015a9fae24e669d29d
youssef665 <yi559668@gmail.com>
2023-04-18 15:48:14 -0700

    something is wrong

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef _function_like_macro_h
-#define  _function_like_macro_h
+#ifndef _function_like_macro_h_
+#define  _function_like_macro_h_
 
 #define ABS(x) ((x) < (0)) ? ((x) * (-1)) : (x)
 
