c32eebcd8c4e7c58f463deb084798d093a020064
youssef665 <yi559668@gmail.com>
2023-04-20 12:45:20 -0700

    i know that there some tests wrong

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include " variadic_functions.h"
+#include "variadic_functions.h"
 
 /**
  * print_strings - a function that prints strings
