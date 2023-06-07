bf769b34539a077b8185b28df6e4604a41b75ac8
youssef665 <yi559668@gmail.com>
2023-06-07 13:37:15 -0700

    try again

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * p - find prime number
+ * @x: an integer number
+ * @y: second integer number
+ *
+ * Return: integer number
+ */
+int p(int x, int y)
+{
+	if (x == y)
+		return (1);
+	else if (x % y == 0)
+		return (0);
+	return (prime2(x, y + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: integer number
+ *
+ * Return: integer number.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (p(n, 2));
+}
