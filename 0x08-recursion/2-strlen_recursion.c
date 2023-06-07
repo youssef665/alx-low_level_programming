e1f01e141bfafc8388508baab1240ff58d3d69e6
youssef665 <yi559668@gmail.com>
2023-06-07 12:16:05 -0700

    i know it is wrong

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that know the lenght of the string
+ * @s: a pointer to character
+ *
+ * Return: an integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int counter = 0;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	else
+	{
+		_strlen_recursion(s + 1);
+
+		counter++;
+	}
+
+	return (counter);
+}
+
