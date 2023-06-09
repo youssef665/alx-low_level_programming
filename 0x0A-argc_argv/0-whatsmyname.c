4784657a9117eda86fd118cb57e1575e14b59dcd
youssef665 <yi559668@gmail.com>
2023-06-09 02:39:14 -0700

    1st try

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a entry point
+ * @argc: an argument counter
+ * @argv: a pointer of arrays of strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+
+	return (0);
+}
