d5d3311371599d15a8b707a470628aa97aa745cb
youssef665 <yi559668@gmail.com>
2023-06-09 12:39:33 -0700

    i think putchar file problem

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,25 +1,13 @@
-#include "main.h"
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0
- *
- * program to print _putchar
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
-int main(void)
+int _putchar(char c)
 {
-	char arr[] = "_putchar";
-	int i = 0;
-
-	while (i < 8)
-	{
-		putchar(arr[i]);
-		i++;
-	}
-
-		putchar('\n');
-
-		return (0);
+	return (write(1, &c, 1));
 }
