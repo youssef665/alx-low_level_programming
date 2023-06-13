41eee254d303a20ee4eaada33293a4bbbd974ea1
youssef665 <yi559668@gmail.com>
2023-06-13 08:21:12 -0700

    code intended mistake

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -25,8 +25,8 @@ char *create_array(unsigned int size, char c)
 
 		if (p != NULL)
 		{
-			 for (i = 0; i < size; i++)
-				*(p + i) = c;
+			for (i = 0; i < size; i++)
+			*(p + i) = c;
 		}
 	}
 
