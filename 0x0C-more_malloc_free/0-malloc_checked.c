1250372c60a9a229f198765ca96109c106cec33a
youssef665 <yi559668@gmail.com>
2023-06-15 12:23:49 -0700

    1st try

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * malloc_checked - a function that allocates memory
+ * @b: a postive int argument
+ *
+ * Return: pointer to the address
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *b = malloc(b);
+
+	if (b != NULL)
+
+	return (b);
+
+	exit(98);
+}
