129ca4e6b07b8d263c4d82d5ac3a89a584a2f2cf
youssef665 <yi559668@gmail.com>
2023-06-19 10:34:56 -0700

    1st try

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog_H
+#define dog_H
+
+
+/**
+ * struct dog - contains dog attributes
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: conatins name ,age and owner of the dog .
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
