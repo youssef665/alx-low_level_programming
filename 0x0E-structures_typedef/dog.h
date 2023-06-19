744cc1e6e2cf068e1ca0bac5226261e6bd420bf4
youssef665 <yi559668@gmail.com>
2023-06-19 12:15:26 -0700

    syntax problem

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,11 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - Typedef for struct dog
  */
-Typdef struct dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
