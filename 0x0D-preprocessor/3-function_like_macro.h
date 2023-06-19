a212d17f02c69d4e27bd8b4955cdd03b5af5527d
youssef665 <yi559668@gmail.com>
2023-06-19 08:41:37 -0700

    try again

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef function_like_macro_H
 #define function_like_macro_H
 
-#define ABS(x) ((x) < (0) ? ((x) * (-1) : ((x) * (1))))
+#define ABS(x) ((X) < (0) ? ((X) * (-1) : ((X) * (1))))
 
 #endif
