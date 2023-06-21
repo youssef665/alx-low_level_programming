4f0b8e5032cc35881525ee09058c7c93abf012cc
youssef665 <yi559668@gmail.com>
2023-06-21 13:34:19 -0700

    return problem in NULL condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,13 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	 int s ;
+	 int s;
 	 int i;
 
 	if (array == NULL ||  cmp == NULL)
 		return (-1);
 	if (size <= 0)
 		return (-1);
-	
 
 	for (i = 0; i < size; i++)
 	{
@@ -29,7 +28,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 			return (i);
 
 	}
-		return (-1);
-	
-	
+	return (-1);
 }
