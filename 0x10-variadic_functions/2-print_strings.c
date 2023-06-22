63cb173418b7668b8596152b15e2ce1ab36de01a
youssef665 <yi559668@gmail.com>
2023-06-22 14:05:42 -0700

    description missing for print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,6 +3,14 @@
 #include "stdlib.h"
 #include "stdio.h"
 
+/**
+ * print_strings - function that prints strings
+ * followed by a new line
+ * @separator: is the string to be printed betweeen strings
+ * @n: number of strings to be printed
+ */
+
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list p;
