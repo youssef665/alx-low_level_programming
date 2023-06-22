9651c0e1adacd87255274866987b53f80e08a3da
youssef665 <yi559668@gmail.com>
2023-06-22 07:46:33 -0700

    1st try

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - a function that sum all the parameters
+ * @n: a constant postive integer
+ *
+ * Return: the sum of numbers or 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list p;
+	int i, sum = 0;
+
+
+	va_start(p, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(p, int);
+	}
+		return (sum);
+}
+
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef variadic_functions_H
+#define variadic_functions_H
+
+int putchar(int c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+
+#endif
