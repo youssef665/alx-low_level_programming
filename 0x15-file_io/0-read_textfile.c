7218e5b9894057580ba28ee9e145fb43d0d00f44
youssef665 <yi559668@gmail.com>
2023-07-10 14:56:18 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
-	bytes = write(STDOUT_FILENO, &but[0], bytes);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
 	close(fd);
 
 	return (bytes);
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+#define READ_BUF_SIZE 1024
 
 
 int putchar(int c);
