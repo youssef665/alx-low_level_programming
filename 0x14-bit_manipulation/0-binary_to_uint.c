0853f9c3b1f6da295779f4597eead44887189033
youssef665 <yi559668@gmail.com>
2023-08-04 09:35:29 -0700

    betty mistake

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - a function that converts a binary number 
+ * binary_to_uint - a function that converts a binary number
  * an unsigned int
  * @b: pointer to character
  *
