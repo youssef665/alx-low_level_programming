79a6b008aaf6efb598a72531806da00d7dfdf425
youssef665 <yi559668@gmail.com>
2023-08-04 09:40:24 -0700

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,24 @@
 #include "main.h"
 
 /**
+ * print_binary - function that prints the binary representation
+ * of number
+ * @n: number to be printed (postive)
  *
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
+}
