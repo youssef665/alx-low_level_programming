720aaa619cd04b13c7ff9cc8c4ff3d568b02e76e
youssef665 <yi559668@gmail.com>
2023-08-07 10:15:50 -0700

    betty mistake

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - function that creates a file
  * @filename: name of file
- * @text_content: the text 
+ * @text_content: the text
  * Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
