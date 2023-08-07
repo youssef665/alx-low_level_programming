55f39ebcc30738a484ac3fb31cd4a029731988d6
youssef665 <yi559668@gmail.com>
2023-08-07 10:14:41 -0700

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: name of file
+ * @text_content: the text 
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f, i;
+
+	if (!filename)
+		return (-1);
+
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (f == -1)
+		return (-1);
+	if (!text_content)
+		text_content = "";
+	for (i = 0; *(text_content + i) != '\0'; i++)
+		;
+
+	write(f, text_content, i);
+	close(f);
+	return (1);
+}
