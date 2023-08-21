0667023c099c51e7261564e9c1f2d87dacd3cc27
youssef665 <yi559668@gmail.com>
2023-08-21 05:16:22 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - afuntion that checks for uppercase character
+ * @c: First opearand
+ *
+ * Return: 1 if c is uppercase else return 0
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * _puts - a function that prints a string
+ *@str: pointer character argument
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != 0)
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar('\n');
+}
